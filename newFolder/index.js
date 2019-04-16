fd8d672c562ddb498108e1470f4a6a459f6aa8cf
Nkululeko <1135124@students.wits.ac.za>
2019-04-16 10:29:24 +0200

    Running on Server

---
+++ newFolder/index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+let path = require('path')
+let express = require('express')
+let app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000)
+console.log('Express server running on port 3000')
