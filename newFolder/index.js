acaf8553ea00c11bf57b2aec805d3f1aa3b195e2
Nkululeko <1135124@students.wits.ac.za>
2019-04-16 11:09:47 +0200

    Routing as Module

--- newFolder/index.js
+++ newFolder/index.js
@@ -3,17 +3,9 @@
 let path = require('path')
 let express = require('express')
 let app = express()
-let mainRouter = express.Router();
+let mainRouter = require('./mainRoutes')
 
-mainRouter.get('/', function (req, res) {
-  res.send('Hello World')
-})
-
-mainRouter.get('/about', function (req, res) {
-  res.sendFile(path.join(__dirname, 'views', 'about.html'))
-})
-
-app.use(mainRouter);
+app.use(mainRouter)
 
 app.listen(3000)
 console.log('Express server running on port 3000')
