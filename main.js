b852308ff7e2175a7026fe7709e352532b90da6f
Nkululeko <1135124@students.wits.ac.za>
2019-04-16 09:58:09 +0200

    Module Text File

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ console.log('Loading a module')
 let mod = require('./mod')
 console.log('Mod:', mod)
 console.log('Done')
+
+let fs = require('fs')
+fs.readFile('./ELEN4010.txt', 'utf8', (err, data) => {
+  if (err) throw err
+  console.log(data)
+})
