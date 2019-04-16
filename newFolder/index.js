742b1f0b64147f5776d4e8415da1d59bf877528a
Nkululeko <1135124@students.wits.ac.za>
2019-04-16 10:56:08 +0200

    Routing

--- newFolder/index.js
+++ newFolder/index.js
@@ -3,10 +3,17 @@
 let path = require('path')
 let express = require('express')
 let app = express()
+let mainRouter = express.Router();
 
-app.get('/', function (req, res) {
+mainRouter.get('/', function (req, res) {
   res.send('Hello World')
 })
+
+mainRouter.get('/about', function (req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
+
+app.use(mainRouter);
 
 app.listen(3000)
 console.log('Express server running on port 3000')
